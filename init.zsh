09e7ae356461430e4aaba2bee50ce81b29fe2ecc
Arash Rouhani <arash@spotify.com>
2014-06-22 12:52:54 +0200

    Don't leak global variables, fixes #2

--- init.zsh
+++ init.zsh
@@ -4,14 +4,16 @@ then
   echo "Put this file in '~/.zsh/antigen-hs/init.zsh' please!"
 fi
 
-local FILE_TO_SOURCE="$HOME/.antigen-hs/antigen-hs.zsh"
-if [[ -f $FILE_TO_SOURCE ]]
-then
-  source $FILE_TO_SOURCE
-else
-  echo "Didn't find file $FILE_TO_SOURCE"
-  echo "Try running antigen-hs-compile"
-fi
+() {
+  local FILE_TO_SOURCE="$HOME/.antigen-hs/antigen-hs.zsh"
+  if [[ -f $FILE_TO_SOURCE ]]
+  then
+    source $FILE_TO_SOURCE
+  else
+    echo "Didn't find file $FILE_TO_SOURCE"
+    echo "Try running antigen-hs-compile"
+  fi
+}
 
 antigen-hs-compile () {
   runghc -i"$HOME/.zsh/antigen-hs/" -- "$HOME/.zsh/MyAntigen.hs"
