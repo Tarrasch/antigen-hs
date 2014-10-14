4b9e91e6a11787b08e26f099019f19c3aab10cc0
Srijan R Shetty <srijan.shetty@gmail.com>
2014-10-14 14:59:43 +0530

    Added remove

--- init.zsh
+++ init.zsh
@@ -4,10 +4,6 @@ then
   echo "Put this file in '~/.zsh/antigen-hs/init.zsh' please!"
 fi
 
-# Get the list of bundles
-list=("${(f)$(< $HOME/.zsh/bundles)}")
-BUNDLES=$(IFS=','; echo "${list[*]}"; IFS=$' \t\n')
-
 () {
   local FILE_TO_SOURCE="$HOME/.antigen-hs/antigen-hs.zsh"
   if [[ -f $FILE_TO_SOURCE ]]
@@ -20,6 +16,10 @@ BUNDLES=$(IFS=','; echo "${list[*]}"; IFS=$' \t\n')
 }
 
 antigen-create() {
+    # Get the list of bundles
+    list=("${(f)$(< $HOME/.zsh/bundles)}")
+    BUNDLES=$(IFS=','; echo "${list[*]}"; IFS=$' \t\n')
+
     [ -e $HOME/.zsh/MyAntigen.hs ] && rm -f $HOME/.zsh/MyAntigen.hs
 
     HEADER='
@@ -56,11 +56,27 @@ antigen-update() {
     antigen-hs-compile
 }
 
+antigen-list() {
+    list=("${(f)$(< $HOME/.zsh/bundles)}")
+
+    length=${#list[*]}
+    for ((i=1; i<=length; i=i+1)); do
+        echo "$i : $list[i]"
+    done
+}
+
 antigen-add() {
     echo "bundle \"$1\"" >> $HOME/.zsh/bundles
     antigen-hs-compile
 }
 
-antigen-list() {
-    IFS=$'\n'; echo "${list[*]}\n"; IFS=$' \t\n'
+antigen-remove() {
+    list=("${(f)$(< $HOME/.zsh/bundles)}")
+    list[$1]=()
+    echo "$list"
+
+    [ -e $HOME/.zsh/bundles ] && rm -f $HOME/.zsh/bundles
+    IFS=$'\n'; echo "${list[*]}" > $HOME/.zsh/bundles; IFS=$' \t\n'
+
+    antigen-hs-compile
 }
